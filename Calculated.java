352724410cc3b9ac8791d5ee08e594449fa41029
jceletherio <eletherio09@gmail.com>
2021-04-24 16:41:45 -0300

    Add files via upload

---
+++ Calculated.java
@@ -0,0 +1,12 @@
+
+public class Calculated {
+	private String type, hex;
+	
+	public Calculated(String type, String hex) {
+		this.type = type;
+		this.hex = hex;
+	}
+	
+	public String getType() {return type;}
+	public String getHex() {return hex;}
+}
--- DigestCalculator.java
+++ DigestCalculator.java
@@ -1,11 +1,16 @@
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
 import java.security.MessageDigest;
 import java.security.*;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.io.*;
 
+
+
 public class DigestCalculator {
+	public static Map<String, ArrayList<Calculated>> dgstMap = new HashMap<String,ArrayList<Calculated>>();
+	
     public static void main(String[] args) throws Exception {
         String tipoDgst = "";
         String pathListaDgst = "";
@@ -24,9 +29,34 @@ public class DigestCalculator {
         }
         File arqListaDgst = new File(pathListaDgst);
         File arqListaArq = new File (pathPastaArquivos);
-        try{
+        
+        fileToMap(arqListaDgst);
+        calcDigest(arqListaArq, tipoDgst);
+        
+    }
+    
+    public static void fileToMap(File f) throws FileNotFoundException, IOException {
+    	try (BufferedReader br = new BufferedReader(new FileReader(f))) {
+    	    String line;
+    	    while ((line = br.readLine()) != null) {
+    	    	ArrayList<Calculated> lc = new ArrayList<>();
+    	    	String[] bline = line.split(" ");
+    	    	String fileName = bline[0];
+    	    	for(int i=1; i<bline.length;i+=2) {
+    	    		Calculated c = new Calculated(bline[i],bline[i+1]);
+    	    		lc.add(c);
+    	    	}
+    	    	dgstMap.put(fileName, lc);
+       	    }
+    	}	
+    }
+    
+    public static void calcDigest(File arqListaArq, String tipoDgst) throws Exception {
+    	try{
             MessageDigest algoritm = MessageDigest.getInstance(tipoDgst);
             File[] arqs = arqListaArq.listFiles();
+            
+            
             for (int i =0;i<arqs.length;i++){
             	InputStream fis =  new FileInputStream(arqs[i]);
             	byte[] buffer = new byte[1024];
@@ -44,15 +74,32 @@ public class DigestCalculator {
                 for (int i1=0; i1 < digest.length; i1++) {
                     calculatedDigest += Integer.toString( ( digest[i1] & 0xff ) + 0x100, 16).substring( 1 );
                 }
-                System.out.println("Arquivo:"+arqs[i].getName());
-                System.out.println( calculatedDigest );
-                                
+                String fileName = arqs[i].getName();
+                Calculated c = new Calculated(tipoDgst, calculatedDigest);
+                
+                
+                /*FileWriter myWriter = new FileWriter(pathListaDgst, true);
+                myWriter.write(arqs[i].getName()+" "+tipoDgst+" "+calculatedDigest +"\n");
+                myWriter.close();*/
+               
             }
+            status("a", null);
         }catch(Exception  algErr){
             //TODO tratamento de erro
             System.out.print("Problema do tipo \t"+algErr);
             throw algErr;
         }
     }
+    
+    public static void status(String name, Calculated c) {
+    	for (String i : dgstMap.keySet()) {
+    		//dgstMap.get(i).clear();
+    		  System.out.println(i);
+    		  for(Calculated i1 : dgstMap.get(i)) {
+    			  System.out.println(i1.getHex());
+    		  }
+    		}
+    }
+    
 }
 
